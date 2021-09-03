b28a351ab634fc7ac38ceb8d4d50f0f0c659044e
qwerty2501 <939468+qwerty2501@users.noreply.github.com>
2021-09-03 22:21:44 +0900

    removed support \r only newline

--- crates/novel-archives-text/src/parser/character/complete.rs
+++ crates/novel-archives-text/src/parser/character/complete.rs
@@ -5,7 +5,6 @@ pub fn newline(input: token::Span) -> nom::IResult<token::Span, token::Span> {
     alt((
         nom::bytes::complete::tag("\n"),
         nom::bytes::complete::tag("\r\n"),
-        nom::bytes::complete::tag("\r"),
     ))(input)
 }
 
@@ -18,6 +17,10 @@ mod tests {
             token::Span::new("hoge"),
             nom::error::ErrorKind::Tag,
         ))))]
+    #[test_case("\r"=> Err(nom::Err::Error(nom::error::Error::new(
+            token::Span::new("\r"),
+            nom::error::ErrorKind::Tag,
+        ))))]
     #[test_case("hoge\nfoo"=> Err(nom::Err::Error(nom::error::Error::new(
             token::Span::new("hoge\nfoo"),
             nom::error::ErrorKind::Tag,
@@ -25,9 +28,6 @@ mod tests {
     #[test_case("\n"=> Ok((new_test_result_span(1, 2, ""),new_test_result_span(0, 1, "\n"))))]
     #[test_case("\nhoge"=> Ok((new_test_result_span(1, 2, "hoge"),new_test_result_span(0, 1, "\n"))))]
     #[test_case("\n\n"=> Ok((new_test_result_span(1, 2, "\n"),new_test_result_span(0, 1, "\n"))))]
-    #[test_case("\r"=> Ok((new_test_result_span(1, 1, ""),new_test_result_span(0, 1, "\r"))))]
-    #[test_case("\rhoge"=> Ok((new_test_result_span(1, 1, "hoge"),new_test_result_span(0, 1, "\r"))))]
-    #[test_case("\r\r"=> Ok((new_test_result_span(1, 1, "\r"),new_test_result_span(0, 1, "\r"))))]
     #[test_case("\r\n"=> Ok((new_test_result_span(2, 2, ""),new_test_result_span(0, 1, "\r\n"))))]
     #[test_case("\r\n\r\n"=> Ok((new_test_result_span(2, 2, "\r\n"),new_test_result_span(0, 1, "\r\n"))))]
     fn newline_works(input: &str) -> nom::IResult<token::Span, token::Span> {
