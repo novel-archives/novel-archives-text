f7c25082de2d4066b488a735c582677bd28523bc
qwerty2501 <939468+qwerty2501@users.noreply.github.com>
2021-09-03 21:29:07 +0900

    implemented newline function

--- crates/novel-archives-text/src/parser/character/complete.rs
+++ crates/novel-archives-text/src/parser/character/complete.rs
@@ -1,36 +1,40 @@
 use super::*;
-pub fn newline(input: &str) -> nom::IResult<&str, token::TokenKind> {
-    match input.len().cmp(&1) {
-        std::cmp::Ordering::Equal => {
-            let c = input.bytes().next().unwrap();
-            if c == b'\n' || c == b'\r' {
-                Ok((&input[1..], token::TokenKind::NewLine))
-            } else {
-                Err(nom::Err::Error(nom::error::Error::new(
-                    input,
-                    nom::error::ErrorKind::Char,
-                )))
-            }
-        }
-        std::cmp::Ordering::Greater => {
-            let mut bytes = input.bytes();
-            let first = bytes.next().unwrap();
-            let second = bytes.next().unwrap();
+use nom::branch::alt;
+pub use nom::character::complete::*;
+pub fn newline(input: token::Span) -> nom::IResult<token::Span, token::Span> {
+    alt((
+        nom::bytes::complete::tag("\n"),
+        nom::bytes::complete::tag("\r\n"),
+        nom::bytes::complete::tag("\r"),
+    ))(input)
+}
 
-            if first == b'\r' && second == b'\n' {
-                Ok((&input[2..], token::TokenKind::NewLine))
-            } else if (first == b'\r' && second != b'\n') || (first == b'\n') {
-                Ok((&input[1..], token::TokenKind::NewLine))
-            } else {
-                Err(nom::Err::Error(nom::error::Error::new(
-                    input,
-                    nom::error::ErrorKind::Char,
-                )))
-            }
-        }
-        _ => Err(nom::Err::Error(nom::error::Error::new(
-            input,
-            nom::error::ErrorKind::Char,
-        ))),
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use test_case::test_case;
+
+    #[test_case("hoge"=> Err(nom::Err::Error(nom::error::Error::new(
+            token::Span::new("hoge"),
+            nom::error::ErrorKind::Tag,
+        ))))]
+    #[test_case("hoge\nfoo"=> Err(nom::Err::Error(nom::error::Error::new(
+            token::Span::new("hoge\nfoo"),
+            nom::error::ErrorKind::Tag,
+        ))))]
+    #[test_case("\n"=> Ok((new_test_result_span(1, 2, ""),new_test_result_span(0, 1, "\n"))))]
+    #[test_case("\nhoge"=> Ok((new_test_result_span(1, 2, "hoge"),new_test_result_span(0, 1, "\n"))))]
+    #[test_case("\n\n"=> Ok((new_test_result_span(1, 2, "\n"),new_test_result_span(0, 1, "\n"))))]
+    #[test_case("\r"=> Ok((new_test_result_span(1, 1, ""),new_test_result_span(0, 1, "\r"))))]
+    #[test_case("\rhoge"=> Ok((new_test_result_span(1, 1, "hoge"),new_test_result_span(0, 1, "\r"))))]
+    #[test_case("\r\r"=> Ok((new_test_result_span(1, 1, "\r"),new_test_result_span(0, 1, "\r"))))]
+    #[test_case("\r\n"=> Ok((new_test_result_span(2, 2, ""),new_test_result_span(0, 1, "\r\n"))))]
+    #[test_case("\r\n\r\n"=> Ok((new_test_result_span(2, 2, "\r\n"),new_test_result_span(0, 1, "\r\n"))))]
+    fn newline_works(input: &str) -> nom::IResult<token::Span, token::Span> {
+        newline(token::Span::new(input))
+    }
+
+    fn new_test_result_span(offset: usize, line: u32, fragment: &str) -> token::Span {
+        unsafe { token::Span::new_from_raw_offset(offset, line, fragment, ()) }
     }
 }
