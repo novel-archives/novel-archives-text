d01d8008f1ddb427e9c716175fe5a1f4fdcfe63b
qwerty2501 <939468+qwerty2501@users.noreply.github.com>
2021-09-05 22:27:19 +0900

    feat: implemented any_newline

--- crates/novel-archives-text/src/parser/character/complete.rs
+++ crates/novel-archives-text/src/parser/character/complete.rs
@@ -1,11 +1,25 @@
 use super::*;
 use nom::branch::alt;
 pub use nom::character::complete::*;
-pub fn lf_or_crlf(input: token::Span) -> nom::IResult<token::Span, token::Span> {
-    alt((
+pub fn any_newline(input: token::Span) -> nom::IResult<token::Span, token::Span> {
+    let result: nom::IResult<token::Span, token::Span> = alt((
         nom::bytes::complete::tag("\n"),
         nom::bytes::complete::tag("\r\n"),
-    ))(input)
+    ))(input);
+    match result {
+        Ok(ok) => Ok(ok),
+        Err(_) => nom::bytes::complete::tag("\r")(input).map(|(input, parsed)| unsafe {
+            (
+                token::Span::new_from_raw_offset(
+                    input.location_offset(),
+                    input.location_line() + 1,
+                    input.fragment(),
+                    (),
+                ),
+                parsed,
+            )
+        }),
+    }
 }
 
 #[cfg(test)]
@@ -17,10 +31,6 @@ mod tests {
             token::Span::new("hoge"),
             nom::error::ErrorKind::Tag,
         ))))]
-    #[test_case("\r"=> Err(nom::Err::Error(nom::error::Error::new(
-            token::Span::new("\r"),
-            nom::error::ErrorKind::Tag,
-        ))))]
     #[test_case("hoge\nfoo"=> Err(nom::Err::Error(nom::error::Error::new(
             token::Span::new("hoge\nfoo"),
             nom::error::ErrorKind::Tag,
@@ -28,10 +38,13 @@ mod tests {
     #[test_case("\n"=> Ok((new_test_result_span(1, 2, ""),new_test_result_span(0, 1, "\n"))))]
     #[test_case("\nhoge"=> Ok((new_test_result_span(1, 2, "hoge"),new_test_result_span(0, 1, "\n"))))]
     #[test_case("\n\n"=> Ok((new_test_result_span(1, 2, "\n"),new_test_result_span(0, 1, "\n"))))]
+    #[test_case("\r"=> Ok((new_test_result_span(1, 2, ""),new_test_result_span(0, 1, "\r"))))]
+    #[test_case("\rhoge"=> Ok((new_test_result_span(1, 2, "hoge"),new_test_result_span(0, 1, "\r"))))]
+    #[test_case("\r\r"=> Ok((new_test_result_span(1, 2, "\r"),new_test_result_span(0, 1, "\r"))))]
     #[test_case("\r\n"=> Ok((new_test_result_span(2, 2, ""),new_test_result_span(0, 1, "\r\n"))))]
     #[test_case("\r\n\r\n"=> Ok((new_test_result_span(2, 2, "\r\n"),new_test_result_span(0, 1, "\r\n"))))]
-    fn lf_or_crlf_works(input: &str) -> nom::IResult<token::Span, token::Span> {
-        lf_or_crlf(token::Span::new(input))
+    fn any_newline_works(input: &str) -> nom::IResult<token::Span, token::Span> {
+        any_newline(token::Span::new(input))
     }
 
     fn new_test_result_span(offset: usize, line: u32, fragment: &str) -> token::Span {
