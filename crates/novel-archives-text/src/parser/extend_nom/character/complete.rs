a526d2dad614046b69f042aba0fd0a31da993094
qwerty2501 <939468+qwerty2501@users.noreply.github.com>
2021-09-06 19:11:00 +0900

    wip implementing kanji

--- crates/novel-archives-text/src/parser/extend_nom/character/complete.rs
+++ crates/novel-archives-text/src/parser/extend_nom/character/complete.rs
@@ -1,5 +1,7 @@
 use super::*;
 use nom::branch::alt;
+use nom::bytes::complete::take_while;
+use nom::bytes::complete::take_while1;
 pub fn any_newline(input: token::Span) -> nom::IResult<token::Span, token::Span> {
     let result: nom::IResult<token::Span, token::Span> = alt((
         nom::bytes::complete::tag("\n"),
@@ -19,6 +21,14 @@ pub fn any_newline(input: token::Span) -> nom::IResult<token::Span, token::Span>
             )
         }),
     }
+}
+
+pub fn kanji0(input: token::Span) -> nom::IResult<token::Span, token::Span> {
+    take_while(is_kanji)(input)
+}
+
+pub fn kanji1(input: token::Span) -> nom::IResult<token::Span, token::Span> {
+    take_while1(is_kanji)(input)
 }
 
 #[cfg(test)]
@@ -44,5 +54,28 @@ mod tests {
     #[test_case("\r\n\r\n"=> Ok((token::test_helper::new_test_result_span(2, 2, "\r\n"),token::test_helper::new_test_result_span(0, 1, "\r\n"))))]
     fn any_newline_works(input: &str) -> nom::IResult<token::Span, token::Span> {
         any_newline(token::Span::new(input))
+    }
+
+    #[test_case("漢字"=> Ok((token::test_helper::new_test_result_span(6, 1, ""),token::test_helper::new_test_result_span(0, 1, "漢字"))))]
+    #[test_case("漢字とひらがな"=> Ok((token::test_helper::new_test_result_span(6, 1, "とひらがな"),token::test_helper::new_test_result_span(0, 1, "漢字"))))]
+    #[test_case("なか漢字なか"=> Err(nom::Err::Error(nom::error::Error::new(
+            token::Span::new("なか漢字なか"),
+            nom::error::ErrorKind::TakeWhile1,
+        ))))]
+    #[test_case("かんじなし"=> Err(nom::Err::Error(nom::error::Error::new(
+            token::Span::new("かんじなし"),
+            nom::error::ErrorKind::TakeWhile1,
+        ))))]
+    fn kanji1_works(input: &str) -> nom::IResult<token::Span, token::Span> {
+        kanji1(token::Span::new(input))
+    }
+    #[test_case("漢字"=> Ok((token::test_helper::new_test_result_span(6, 1, ""),token::test_helper::new_test_result_span(0, 1, "漢字"))))]
+    #[test_case("漢字とひらがな"=> Ok((token::test_helper::new_test_result_span(6, 1, "とひらがな"),token::test_helper::new_test_result_span(0, 1, "漢字"))))]
+    #[test_case("なか漢字なか"=> Ok((token::test_helper::new_test_result_span(0, 1, "なか漢字なか"),token::test_helper::new_test_result_span(0, 1, ""))))]
+    #[test_case("かんじなし"=> Ok((token::test_helper::new_test_result_span(0, 1, "かんじなし"),token::test_helper::new_test_result_span(0, 1, ""))))]
+    #[test_case("alphabet"=> Ok((token::test_helper::new_test_result_span(0, 1, "alphabet"),token::test_helper::new_test_result_span(0, 1, ""))))]
+    #[test_case("01224"=> Ok((token::test_helper::new_test_result_span(0, 1, "01224"),token::test_helper::new_test_result_span(0, 1, ""))))]
+    fn kanji0_works(input: &str) -> nom::IResult<token::Span, token::Span> {
+        kanji0(token::Span::new(input))
     }
 }
--- crates/novel-archives-text/src/parser/token/complete.rs
+++ crates/novel-archives-text/src/parser/token/complete.rs
@@ -5,6 +5,10 @@ pub fn newline(input: Span) -> IResult {
     Ok(complete::any_newline(input).map(|(input, parsed)| (input, Token::NewLine(parsed)))?)
 }
 
+pub fn kanji(input: Span) -> IResult {
+    Ok(complete::kanji1(input).map(|(input, parsed)| (input, Token::Kanji(parsed)))?)
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -21,5 +25,13 @@ mod tests {
     #[test_case("\r\n\r\n"=> Ok((token::test_helper::new_test_result_span(2, 2, "\r\n"),Token::NewLine(token::test_helper::new_test_result_span(0, 1, "\r\n")))))]
     fn newline_works(input: &str) -> IResult {
         newline(token::Span::new(input))
+    }
+
+    #[test_case("漢字"=> Ok((token::test_helper::new_test_result_span(6, 1, ""),Token::Kanji(token::test_helper::new_test_result_span(0, 1, "漢字")))))]
+    #[test_case("漢字とひらがな"=> Ok((token::test_helper::new_test_result_span(6, 1, "とひらがな"),Token::Kanji(token::test_helper::new_test_result_span(0, 1, "漢字")))))]
+    #[test_case("なか漢字なか"=> Err(Error::Nom(token::test_helper::new_test_result_span(0, 1, "なか漢字なか"),nom::error::ErrorKind::TakeWhile1)))]
+    #[test_case("かんじなし"=> Err(Error::Nom(token::test_helper::new_test_result_span(0, 1, "かんじなし"),nom::error::ErrorKind::TakeWhile1)))]
+    fn kanji_works(input: &str) -> IResult {
+        kanji(token::Span::new(input))
     }
 }
